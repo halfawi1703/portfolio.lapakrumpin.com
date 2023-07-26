126bdb943db56d1eb79452073930e7619b83a385
halfawi1703 <halfawi1703@gmail.com>
2023-07-26 13:32:44 +0700

    first commit

---
+++ application/views/welcome_message.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>LapakRumpin.com</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto'>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <style>
+    html,
+    body,
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6 {
+      font-family: "Roboto", sans-serif
+    }
+  </style>
+</head>
+
+<body class="w3-light-grey">
+
+  <!-- Page Container -->
+  <div class="w3-content w3-margin-top" style="max-width:1400px;">
+
+    <!-- The Grid -->
+    <div class="w3-row-padding">
+
+      <!-- Left Column -->
+      <div class="w3-third">
+
+        <div class="w3-white w3-text-grey w3-card-4">
+          <div class="w3-display-container">
+            <img src="<?= base_url('assets/logo/') ?>Logo-v1.png" style="width:100%" alt="Avatar">
+            <div class="w3-display-bottomleft w3-container w3-text-white">
+              <h2>Halfawi</h2>
+            </div>
+          </div>
+          <div class="w3-container">
+            <p><i class="fa fa-briefcase fa-fw w3-margin-right w3-large w3-text-teal"></i>Web Developer</p>
+            <p><i class="fa fa-home fa-fw w3-margin-right w3-large w3-text-teal"></i>Jakarta, Indonesia</p>
+            <p><i class="fa fa-envelope fa-fw w3-margin-right w3-large w3-text-teal"></i>halfawi1703@mail.com</p>
+            <p><i class="fa fa-phone fa-fw w3-margin-right w3-large w3-text-teal"></i>082113445443</p>
+            <hr>
+
+            <p class="w3-large"><b><i class="fa fa-asterisk fa-fw w3-margin-right w3-text-teal"></i>Skills</b></p>
+            <p>PHP, HTML & CSS</p>
+            <div class="w3-light-grey w3-round-xlarge w3-small">
+              <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:90%">90%</div>
+            </div>
+            <p>RESTful APIs</p>
+            <div class="w3-light-grey w3-round-xlarge w3-small">
+              <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:80%">80%</div>
+            </div>
+            <p>Framework CodeIgniter</p>
+            <div class="w3-light-grey w3-round-xlarge w3-small">
+              <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:90%">90%</div>
+            </div>
+            <p>Framework Bootstraps</p>
+            <div class="w3-light-grey w3-round-xlarge w3-small">
+              <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:80%">80%</div>
+            </div>
+            <p>Framework Laravel</p>
+            <div class="w3-light-grey w3-round-xlarge w3-small">
+              <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:60%">60%</div>
+            </div>
+            <p>Mysql</p>
+            <div class="w3-light-grey w3-round-xlarge w3-small">
+              <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:80%">
+                <div class="w3-center w3-text-white">80%</div>
+              </div>
+            </div>
+            <p>Jquery & JavaScript</p>
+            <div class="w3-light-grey w3-round-xlarge w3-small">
+              <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:75%">
+                <div class="w3-center w3-text-white">75%</div>
+              </div>
+            </div>
+            <p>Metabase</p>
+            <div class="w3-light-grey w3-round-xlarge w3-small">
+              <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:60%">60%</div>
+            </div>
+            <br>
+
+            <p class="w3-large w3-text-theme"><b><i class="fa fa-globe fa-fw w3-margin-right w3-text-teal"></i>Languages</b></p>
+            <p>Indonesia</p>
+            <div class="w3-light-grey w3-round-xlarge">
+              <div class="w3-round-xlarge w3-teal" style="height:24px;width:100%"></div>
+            </div>
+            <p>English</p>
+            <div class="w3-light-grey w3-round-xlarge">
+              <div class="w3-round-xlarge w3-teal" style="height:24px;width:55%"></div>
+            </div>
+            <br>
+          </div>
+        </div><br>
+
+        <!-- End Left Column -->
+      </div>
+
+      <!-- Right Column -->
+      <div class="w3-twothird">
+
+        <div class="w3-container w3-card w3-white w3-margin-bottom">
+          <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-suitcase fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Work Experience</h2>
+          <div class="w3-container">
+            <h5 class="w3-opacity"><b>Web Developer / PT. Epicultura</b></h5>
+            <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Apr 2023 - <span class="w3-tag w3-teal w3-round">Current</span></h6>
+            <p>
+            <ul>
+              <li>Create dynamic web.</li>
+              <li>Creates well-engineered source code that conforms to accepted web standards.</li>
+              <li>Ability to work well under pressure and manage time effectively.</li>
+              <li>Ability to work independently or in a team.</li>
+            </ul>
+            </p>
+            <hr>
+          </div>
+          <div class="w3-container">
+            <h5 class="w3-opacity"><b>Senior Web Developer / PT. Top Karir Indonesia</b></h5>
+            <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Okt 2019 - Feb 2023</h6>
+            <p>
+            <ul>
+              <li>Create website-based on applications using languages program PHP (Codeigniter 3.0 or above)</li>
+              <li>Conduct innovations related to existing or future products, both in technology and skills</li>
+              <li>Assist the junior team to work on projects </li>
+              <li>Coordinate with team Product and UI/UX to discuss about detail and UI/UX system</li>
+              <li>Coordinate with team DBA and APPS for create structure DB and needs API</li>
+              <li>Maintenance of products that have been developed</li>
+              <li>Upgrade and repair existing programs, library, and plugin</li>
+              <li>Develop robust testing methods to minimize bugs using several testing frameworks (unit test/functional test/etc.)</li>
+              <li>Develop front end and back end website applications based on UI/UX, Concept, and timeline</li>
+              <li>Develop API (restful) for needs team APPS</li>
+              <li>Responsible for testing, optimization, debugging, maintenance, logic, solved problem (technical) and code reviewing</li>
+            </ul>
+            </p>
+            <hr>
+          </div>
+          <div class="w3-container">
+            <h5 class="w3-opacity"><b>Implementation Consultant / PT Indodev Niaga Internet</b></h5>
+            <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Mar 2019 - Jul 2019</h6>
+            <p>
+            <ul>
+              <li>Conducting assessments of current project capacities and timelines. Page 1 of 2</li>
+              <li>Performing implementation planning and setup activities for projects.</li>
+              <li>Developing sound business practices and procedures for the project.</li>
+              <li>Directing team members as needed to ensure successful project implementation.</li>
+              <li>Overseeing the daily business operations.</li>
+              <li>Providing project-related insight and advice to other team members.</li>
+              <li>Liaising with management and stakeholders on project details and deadlines.</li>
+              <li>Training and onboarding new team members as required.</li>
+              <li>Presenting feedback and suggestions relating to the implementation of assigned projects.</li>
+              <li>Identifying, reporting, and resolving key project issues.</li>
+              <li>Ensuring that all project and implementation related documents are up to date.</li>
+            </ul>
+            </p><br>
+          </div>
+        </div>
+
+        <div class="w3-container w3-card w3-white">
+          <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Education</h2>
+          <div class="w3-container">
+            <h5 class="w3-opacity"><b>STMIK Jakarta STI&K</b></h5>
+            <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2010 - 2014</h6>
+            <p>Bachelor's degree, Manajemen Sistem Informasi</p>
+            <hr>
+          </div>
+        </div>
+
+        <!-- End Right Column -->
+      </div>
+
+      <!-- End Grid -->
+    </div>
+
+    <!-- End Page Container -->
+  </div>
+
+  <!-- Footer. This section contains an ad for W3Schools Spaces. You can leave it to support us. -->
+  <footer class="w3-container w3-teal w3-center w3-margin-top">
+    <p>Find me on social media.</p>
+    <i class="fa fa-facebook-official w3-hover-opacity"></i>
+    <i class="fa fa-instagram w3-hover-opacity"></i>
+    <i class="fa fa-linkedin w3-hover-opacity"></i>
+    <p class="w3-small">
+      &copy; Copyright <strong><span>Halfawi</span></strong>. All Rights Reserved
+    </p>
+   </footer>
+
+</body>
+
+</html>
